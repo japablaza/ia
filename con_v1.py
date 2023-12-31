d928cda0b4a87c5509b4e6d0a0576817f26e8deb
Jose Apablaza <japablaza@gmail.com>
2023-12-31 07:23:56 -0600

    Create an article and show the candidates

--- con_v1.py
+++ con_v1.py
@@ -15,9 +15,16 @@ for model in genai.list_models():
         print(model.name)
 
 mod_gem_pro = genai.GenerativeModel('gemini-pro')
-response = mod_gem_pro.generate_content("who is the most advance civilization?")
+response = mod_gem_pro.generate_content("Crear un artículo original de un máximo de 5 párrafos tomando como base el artículo. Tu respuesta tiene que estar en español, debes entregar 2 URL de imágenes reales de alta resolución y adicionalmente debe venir optimizado el SEO del artículo con enlaces externos para poder maximizar las visitas de búsqueda. Al final debes agregar la fuente del artículo: https://towardsdatascience.com/preparing-for-climate-change-with-an-ai-assistant-cdceb5ce4426")
 
 # Check if it was blocked due to saftey concerns
 #print(response.prompt_feedback)
 
-print(response.text)
+# print(response.text)
+print(response.candidates)
+
+articulo = response.candidates
+
+# titulo = articulo[articulo.find("**Titulo**")+1:articulo.find(" ")]
+
+# print(titulo)
