19bcf551bd8ffc40a2bd47a9dc17752c5a5c14b0
Jose Apablaza <japablaza@gmail.com>
2024-01-04 23:19:05 -0600

    Send response to text file

--- con_v1.py
+++ con_v1.py
@@ -14,17 +14,32 @@ for model in genai.list_models():
     if 'generateContent' in model.supported_generation_methods:
         print(model.name)
 
+consulta = """Crear artículo original de un máximo de 10 párrafos tomando como base la URL compartida. 
+Tu respuesta tiene que estar en español, debe contener 2 imágenes y el SEO debe estar optimizado con al menos 
+5 enlaces externos para poder maximizar las visitas de búsqueda. 
+Al final debes agregar la fuente del artículo: https://towardsdatascience.com/preparing-for-climate-change-with-an-ai-assistant-cdceb5ce4426
+"""
+
 mod_gem_pro = genai.GenerativeModel('gemini-pro')
-response = mod_gem_pro.generate_content("Crear un artículo original de un máximo de 5 párrafos tomando como base el artículo. Tu respuesta tiene que estar en español, debes entregar 2 URL de imágenes reales de alta resolución y adicionalmente debe venir optimizado el SEO del artículo con enlaces externos para poder maximizar las visitas de búsqueda. Al final debes agregar la fuente del artículo: https://cfpub.epa.gov/si/si_public_record_report.cfm?dirEntryId=161789&Lab=NHEERL&showcriteria=2&keyword=aliens&timstype=&timssubtypeid=&epanumber=&ombcat=Any&datebeginpublishedpresented=&dateendpublishedpresented=&datebeginupdated=&dateendupdated=&deid=&personname=&personid=&role=Any&journalname=&journalid=&publishername=&publisherid=&sortby=relevance&count=50")
+response = mod_gem_pro.generate_content(consulta)
 
 # Check if it was blocked due to saftey concerns
 #print(response.prompt_feedback)
 
-print(response.text))
-print(response.candidates)
+print(type(response.parts))
+print(type(response.parts))
+print(type(response))
+# print(response.text)
+print(type(response))
+print(type(response.text))
+# print(response.candidates)
+# print(type(response.candidates))
+# articulo = response.candidates
 
-articulo = response.candidates
+# Trying to find text
+# titulo = articulo[articulo.find("Título")+1:articulo.find(" ")]
 
-titulo = articulo[articulo.find("Título")+1:articulo.find(" ")]
+# print(titulo)
 
-print(titulo)
+# with open("respuesta6.txt", 'w') as file:
+#     file.write(response.parts)
