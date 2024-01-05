e21cc38490104b6e26291943f5e843abb04d3be2
Jose Apablaza <japablaza@gmail.com>
2024-01-05 15:02:32 -0600

    POST function

--- conn_web.py
+++ conn_web.py
@@ -2,10 +2,52 @@
 
 import requests
 import os
+import json
+import pprint
 
-user_endpoint = os.environ.get("joomla_user")
+# user_endpoint = os.environ.get("joomla_user")
 passwd_endpoint = os.environ.get("joomla_api")
 base_url = "https://beta.futuropresente.org"
-users_endpoint = "/api/index.php/v1/users"
+base_path = "/api/index.php/v1"
+# end_point = "/users"
+end_point = "/content/articles/146"
 
-print(passwd_endpoint)
+url = base_url + end_point
+
+
+url = base_url + base_path + end_point
+
+payload = {}
+headers = {
+  'X-Joomla-Token': passwd_endpoint
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+pp = pprint.PrettyPrinter(indent=4)
+
+pp.pprint(response.text)
+
+pprint.pprint(response.text)
+
+def post_article(title, alias, articletext, catid):
+
+    url = "https://beta.futuropresente.org/api/index.php/v1/content/articles"
+
+    payload = json.dumps({
+      "alias": alias,
+      "articletext": articletext,
+      "catid": catid,
+      "language": "*",
+      "metadesc": "",
+      "metakey": "",
+      "title": title
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'X-Joomla-Token': passwd_endpoint
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
